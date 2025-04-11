0592588177e62e7bc232d6d46edad6a5d34e2ebd
JVictorCastro-22 <joao.souzacastro525@gmail.com>
2025-04-11 19:36:41 +0000

    inserir o if e else para comparar pib per capita

--- desafoSupertruco.c
+++ desafoSupertruco.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+/*nome: manaus
+população:2279686 
+aréa em km²:114010.92
+pib:45782.75
+pontos turisticos:5
+********************
+Nome: Bahia
+População:  14850513 
+Área em km²: 564760.429
+PIB: 402646605640
+pontos turisticos:5*/
 
 int main() {
  //declarar variáveis para a primeira carta
 char estado1;
 char codigo1[4];
 char nome1[20];
-int populacao1;
+unsigned int populacao1;
 float area1;
 float pib1;
 int npt1; //número de pontos turísticos
 float dp1; // Densidade Populacional
-float pibpc1; // PIB per Capita:
-float SuperPoder1;
+float pibpc1; // PIB per Capita
+
 
 // Ler variáveis para a primeira carta
 
@@ -27,7 +38,7 @@ printf("Digite o nome da cidade:\n");
 scanf("%s", nome1);
 
 printf("Digite a população da cidade:\n");
-scanf("%d", &populacao1);
+scanf("%u", &populacao1);
 
 printf("Digite Digite a área da cidade (em km²):\n");
 scanf("%f", &area1);
@@ -41,7 +52,6 @@ scanf("%d", &npt1);
 dp1 = populacao1 / area1;
 pibpc1 = pib1 / populacao1; 
 
-SuperPoder1 = populacao1 + area1 + pib1 + npt1 + pibpc1;
 
 
 printf("\n Próxima carta:\n");
@@ -50,13 +60,13 @@ printf("\n Próxima carta:\n");
 char estado2;
 char codigo2[4];
 char nome2[20];
-int populacao2;
+unsigned int populacao2;
 float area2;
 float pib2;
 int npt2; //número de pontos turísticos
 float dp2; // Densidade Populacional
 float pibpc2; // PIB per Capita:
-float SuperPoder2;
+
 
 printf("\n Digite os dados da primeira carta.\n");
 // Ler variáveis para a segunda carta
@@ -70,7 +80,7 @@ printf("Digite o nome da cidade:\n");
 scanf("%s", nome2);
 
 printf("Digite a população da cidade:\n");
-scanf("%d", &populacao2);
+scanf("%u", &populacao2);
 
 printf("Digite Digite a área da cidade (em km²):\n");
 scanf("%f", &area2);
@@ -82,9 +92,7 @@ printf("Digite o número de pontos turísticos da cidade:\n");
 scanf("%d", &npt2);
 
 dp2 = populacao2 / area2;
-pibpc2 = pib2 / populacao2;
-
-SuperPoder2 = populacao2 + area2 + pib2 + npt2 + pibpc2;
+pibpc2 = pib2 / populacao2; 
 
 //Exibir dados da primeira carta
 
@@ -92,12 +100,12 @@ printf("\n Carta 1:\n");
 printf("Estado: %c\n", estado1);
 printf("Código: %s\n", codigo1);
 printf("Nome da cidade: %s\n", nome1);
-printf("População:%d\n", populacao1);
+printf("População:%u\n", populacao1);
 printf("Área da cidade em: %2.f km²\n", area1);
-printf("PIB: R$%.2f Bilhões\n", pib1);
+printf("PIB: R$%.2f \n", pib1);
 printf("Número de pontos turísticos: %d\n", npt1);
 printf("Densidade Populacional: %.2f hab/km²\n",dp1);
-printf("PIB per Capita: R$%.2f Bilhões\n",pibpc1);
+printf("PIB per Capita: R$%.2f\n",pibpc1);
 
 //Exibir dados da segunda carta
 
@@ -105,15 +113,19 @@ printf("\n Carta 2:\n");
 printf("Estado: %c\n", estado2);
 printf("Código: %s\n", codigo2);
 printf("Nome da cidade: %s\n", nome2);
-printf("População: %d\n", populacao2);
+printf("População: %u\n", populacao2);
 printf("Área da cidade em: %2.f km²\n", area2);
-printf("PIB: R$%.2f Bilhões\n", pib2);
+printf("PIB: R$%.2f\n", pib2);
 printf("Número de pontos turísticos: %d\n", npt2);
 printf("Densidade Populacional: %.2f hab/km²\n",dp2);
-printf("PIB per Capita: R$%.2f Bilhões \n",pibpc2);   
+printf("PIB per Capita: R$%.2f\n",pibpc2);   
 
-printf("\nResultado do vencedor:\n");
-printf("Empate: %s\n", (SuperPoder1 == SuperPoder2) ? "Sim" : "Não");
-printf("A Carta %d é a vencedora!\n", (SuperPoder1 > SuperPoder2) ? 1 : 2);
+if(pibpc1 > pibpc2){
+    printf("Vitória da Carta 1\n");
+}else{
+    printf("Vitória da Carta 2\n");
+}
+
+
 return 0;   
 }
